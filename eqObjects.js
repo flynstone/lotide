29152a53cdbdbd5fa77a50605760e00f77430557
Julien Lacroix <flynstone@live.ca>
2021-11-24 22:18:51 +0000

    eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,63 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (first, second) {
+  let array = first.length;
+
+  if (first.length < second.length) {
+    array = second.length;
+  }
+
+  for (let i = 0; i < array; i++) {
+
+    if (Array.isArray(first[i]) && Array.isArray(second[i])) {
+      for (let j = 0; j < first[i].length; j++) {
+        if (first[i][j] !== second[i][j]) {
+          return false;
+        }
+      }
+    } else if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+
+    for (let key in object1) {
+      if (object1[key] instanceof Object) {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
