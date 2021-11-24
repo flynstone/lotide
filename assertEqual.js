ed1e4e9d212d23ef342bf712b811e522db472b23
Julien Lacroix <flynstone@live.ca>
2021-11-24 02:50:38 +0000

    add eqArrays.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+    return console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+    return console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (first, second) {
+  let array = first.length;
+
+  if (first.length < second.length) {
+    array = second.length;
+  }
+
+  for (let i = 0; i < array; i++) {
+
+    if (Array.isArray(first[i]) && Array.isArray(second[i])) {
+      for (let j = 0; j < first[i].length; j++) {
+        if (first[i][j] !== second[i][j]) {
+          return false;
+        }
+      }
+    } else if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
