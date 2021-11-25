0f5c3a6518f2331f97d37efd092a9fbb62f3d2aa
Julien Lacroix <flynstone@live.ca>
2021-11-25 23:38:27 +0000

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, cb) => {
+  for (const o in obj) {
+    if (cb(obj[o])) {
+      return o;
+    }
+  }
+}
+
+
+// Implement the function findKey which takes in an object and a callback. It should scan the object and return 
+// the first key for which the callback returns a truthy value.If no key is found, then it should return undefined.
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
