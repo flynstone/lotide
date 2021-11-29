3ba5dec40fe267a57c98ab38ef0c949ab350ce5c
Julien Lacroix <flynstone@live.ca>
2021-11-29 15:10:41 +0000

    refactored head.js and assertEqual.js

--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function (item) {
   return item[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
