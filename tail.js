0ed7980aa780b98c0c77bd82cb53fcf27ac550eb
Julien <flynstone@live.ca>
2021-11-30 02:12:57 +0000

    commit tail.js edit

--- tail.js
+++ tail.js
@@ -1,21 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function(items) {
   return items.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+modules.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
